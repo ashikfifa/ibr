e23404e8ec8d48e6dcfb8d7a7640061c96e01c09
ashikfifa <ashikahmedfifa@gmail.com>
2023-02-06 19:14:53 +0600

    sidebar added

--- src/components/Page2/Page2.js
+++ src/components/Page2/Page2.js
@@ -1,12 +1,14 @@
+import Navbar from "../Home/Navbar/Navbar";
 import Leftsidebar from "./Left-sidebar/Leftsidebar";
 import Rightsidebar from "./Right-sidebar/Rightsidebar";
 
 function Page2() {
   return (
     <div className="Page2">
-      <Rightsidebar/>
-      <Leftsidebar/>
-      
+      <Navbar />
+      <Rightsidebar />
+
+      <Leftsidebar />
     </div>
   );
 }
