2265bc01bef8a59a149df9f6a57919df96e2d52f
ashikfifa <ashikahmedfifa@gmail.com>
2023-02-06 19:15:00 +0600

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import Home from "./components/Home/Home";
 import Page2 from "./components/Page2/Page2";
-
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <Page2/>
+      <Routes>
+        {/* <Route path="/" element={<Navigation />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/file-uploads" element={<Page2 />} />
+      </Routes>
     </div>
   );
 }
